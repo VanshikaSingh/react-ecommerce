d9c95951d16ec03d500fc3615fb2cb210ace03c9
Vanshika Singh <vn975950@dal.ca>
2021-02-09 15:14:44 -0400

    added second page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import Main from './pages/Main';
+import Main2 from "./pages/Main2";
 
 function App() {
   return (
-    <Main/>
+    <Router>
+     <Switch><Link to="/Main">First Page</Link></Switch> 
+     <br></br>
+     <Switch><Link to="/Main2">Second Page</Link></Switch>
+      
+    
+            <Route path="/Main" component={Main}></Route>
+            <Route path="/Main2" component={Main2}></Route>      
+        </Router>
   );
 }
 
